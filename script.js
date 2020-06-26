75b15a7f63d0e48c37c7e13a2e28c259b32beedb
Iman Feisali <iman.feisali@gmail.com>
2020-06-26 04:53:44 +0100

    level 100 done

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
 //You can edit ALL of the code here
+const getBody = document.querySelector("body");
+let makeDiv = document.createElement("div");
+makeDiv.id = "main";
+getBody.appendChild(makeDiv);
+const allEpisodes = getAllEpisodes();
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function pad2(number) {
+  return (number < 10 ? "0" : "") + number;
 }
+
+function makePageForEpisodes(episodeList) {
+  // const rootElem = document.getElementById("root");
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for (let i = 0; i < episodeList.length; i++) {
+    const makeCard = document.createElement("div");
+    makeCard.id = "card";
+    makeDiv.appendChild(makeCard);
+    const writeTitle = document.createElement("h2");
+    writeTitle.id = "title";
+    makeCard.appendChild(writeTitle);
+    writeTitle.innerHTML = `${allEpisodes[i].name}: S${pad2(
+      allEpisodes[i].season
+    )}E${allEpisodes[i].number}`;
+    const makeImage = document.createElement("img");
+    makeImage.id = "image";
+    makeCard.appendChild(makeImage);
+    makeImage.src = allEpisodes[i].image.medium;
+    const makeSummary = document.createElement("div");
+    makeSummary.id = "summary";
+    makeCard.appendChild(makeSummary);
+    makeSummary.innerHTML = "summary: " + allEpisodes[i].summary;
+  }
+}
+
+// function getEpisodes() {
+
+// }
+// console.log(allEpisodes[]);
 
 window.onload = setup;
